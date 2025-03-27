71d387f06107bbfb0354b14aa21a9ce72e62311d
Justus Kraft <hallo@justuskraft.de>
2025-03-27 21:12:35 +0100

    docs: add example pages

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
 import browserslist from "browserslist";
 import { browserslistToTargets } from "lightningcss";
-
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import cssClassChecker from './vite-css-class-checker';
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        breakout: resolve(__dirname, 'examples/breakout/index.html'),
+      },
+    },
+  },
   css: {
     transformer: "lightningcss",
     lightningcss: {
